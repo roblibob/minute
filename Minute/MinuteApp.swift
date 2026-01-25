bd2708f8f9763764b807aab453ad68a792abf11f
Robert Holst <roblibob@gmail.com>
2026-01-25 13:34:46 +0100

    clean up menu bar options

--- Minute/MinuteApp.swift
+++ Minute/MinuteApp.swift
@@ -5,11 +5,13 @@
 //  Created by Robert Holst on 12/19/25.
 //
 
+import AppKit
 import Sparkle
 import SwiftUI
 
 @main
 struct MinuteApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var appState = AppNavigationModel()
     @StateObject private var updaterViewModel: UpdaterViewModel
     private let updaterController: SPUStandardUpdaterController
@@ -33,6 +35,8 @@ struct MinuteApp: App {
         .windowStyle(.hiddenTitleBar)
         .windowToolbarStyle(.unifiedCompact)
         .commands {
+            CommandGroup(replacing: .newItem) {
+            }
             CommandGroup(replacing: .appSettings) {
                 Button("Settings…") {
                     appState.showSettings()
@@ -41,6 +45,25 @@ struct MinuteApp: App {
             }
             CommandGroup(after: .appInfo) {
                 CheckForUpdatesView(model: updaterViewModel)
+            }
+        }
+    }
+}
+
+private final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        removeTopLevelMenuItems(titles: ["Edit", "View"])
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+
+    private func removeTopLevelMenuItems(titles: [String]) {
+        guard let mainMenu = NSApp.mainMenu else { return }
+        for title in titles {
+            if let item = mainMenu.items.first(where: { $0.title == title }) {
+                mainMenu.removeItem(item)
             }
         }
     }
